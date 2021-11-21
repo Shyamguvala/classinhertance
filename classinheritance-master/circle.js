5a6c0c53e3f510e4f71d56d439e3817aa6f61297
Shyamguvala <93509142+Shyamguvala@users.noreply.github.com>
2021-11-21 12:54:13 +0530

    Add files via upload

---
+++ classinheritance-master/circle.js
@@ -0,0 +1,17 @@
+circle.js// import using require
+const Shape =require("./shape")
+
+// declare class
+class Circle extends Shape{
+  super(radius){
+    this.radius= radius;
+  }
+
+     calculateArea(){
+    console.log('Area');
+     }
+  }
+
+
+// export class using module.exports
+  module.exports = Circle;
